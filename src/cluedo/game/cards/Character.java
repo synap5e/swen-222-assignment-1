30429311ebb1128d7e5f56256395f120a0104c04
Simon Pinfold <mail@synap5e.info>
2014-08-05 13:17:49 +1200

    Removed stuff that shouldn't be there

--- src/cluedo/game/cards/Character.java
+++
@@ -1,9 +0,0 @@
-package cluedo.game.cards;
-
-public class Character extends Card {
-
-	public Character(String name) {
-		super(name);
-	}
-
-}
--- src/cluedo/game/cards/Weapon.java
+++
@@ -1,9 +0,0 @@
-package cluedo.game.cards;
-
-public class Weapon extends Card {
-
-	public Weapon(String name) {
-		super(name);
-	}
-
-}
