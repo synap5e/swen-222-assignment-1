c6bff6544afb83862d1f700025b5672c19111f5f
Simon Pinfold <code@gilbertpinfold.co.nz>
2014-08-08 13:49:45 +1200

    * Added loading doorways to the model * Removed direction of neighbours

--- src/cluedo/game/board/Board.java
+++ src/cluedo/game/board/Board.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
-import cluedo.game.board.Location.Direction;
 import cluedo.util.json.JsonEntity;
 import cluedo.util.json.JsonList;
 import cluedo.util.json.JsonNumber;
@@ -48,14 +47,17 @@ public class Board {
 		JsonObject roomsDef = ((JsonObject) defs.get("rooms"));
 		JsonObject weaponsDef = (JsonObject) defs.get("weapons");
 		JsonObject characterDefs = (JsonObject) defs.get("characters");
-		
+		JsonList doorwayDefs = (JsonList) defs.get("doorways");
+
 		Map<Double, Room> roomKeys = new HashMap<Double, Room>();
+		Map<String, Room> roomNames = new HashMap<String, Room>();
 		for (String name : roomsDef.keys()){
 			JsonObject roomDef = (JsonObject) roomsDef.get(name);
 			Double key = ((JsonNumber)roomDef.get("key")).value();
 			Room room = new Room(name);
 			rooms.add(room);
 			roomKeys.put(key, room);
+			roomNames.put(name, room);
 		}
 		
 		Map<Double, Token> startLocations = new HashMap<Double, Token>();
@@ -76,12 +78,20 @@ public class Board {
 			startLocations.put(roomKeyList.get(random.nextInt(roomKeyList.size())), w);
 		}
 
-		createBoard(rows, roomsDef, roomKeys, startLocations);
-		
-	
+		Map<Double, Room> doorways = new HashMap<Double, Room>();
+		for (JsonEntity doorway : doorwayDefs){
+			JsonList doorwayDef = (JsonList) doorway;
+
+			Room room = roomNames.get(((JsonString)doorwayDef.get(1)).value());
+			doorways.put(((JsonNumber)doorwayDef.get(0)).value(), room);
+		}
+
+		createBoard(rows, roomsDef, roomKeys, startLocations, doorways);
+
 	}
 
-	private void createBoard(JsonList rows, JsonObject roomsDef, Map<Double, Room> roomKeys, Map<Double, Token> startLocations) {
+	private void createBoard(	JsonList rows, JsonObject roomsDef, Map<Double, Room> roomKeys,
+								Map<Double, Token> startLocations, Map<Double, Room> doorways) {
 		int y = 0;
 		for (JsonEntity row : rows){
 			int x = 0;
