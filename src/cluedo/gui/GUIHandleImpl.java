33b6e30cdcaf37ec80d82677ca3ccd6d426f55e0
Jamigrith <james@greenwood.net.nz>
2014-08-10 15:09:30 +1200

    Implemented method for asking for player names.

--- src/cluedo/gui/GUIHandleImpl.java
+++ src/cluedo/gui/GUIHandleImpl.java
@@ -1,16 +1,22 @@
 package cluedo.gui;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JSpinner;
+import javax.swing.JTextField;
 import javax.swing.SpinnerModel;
 import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import cluedo.game.board.Card;
 import cluedo.game.board.Character;
@@ -22,6 +28,7 @@ import cluedo.game.board.Weapon;
 public class GUIHandleImpl implements GUIHandle {
 
 	private boolean wait = false;
+	private int numberOfPlayers;
 	
 	@Override
 	public int getNumberOfPlayers(int min, int max) {
@@ -55,13 +62,70 @@ public class GUIHandleImpl implements GUIHandle {
 		}
 		
 		dialog.dispose();
-		return numberOfPlayersModel.getNumber().intValue();
+		numberOfPlayers = numberOfPlayersModel.getNumber().intValue();
+		return numberOfPlayers;
 	}
 
 	@Override
 	public List<String> getHumanNames() {
-		// TODO Auto-generated method stub
-		return null;
+		JDialog dialog = new JDialog();
+
+		dialog.setLayout(new GridLayout(numberOfPlayers+1, 2));
+		
+		List<JTextField> names = new ArrayList<JTextField>();
+		List<JCheckBox> ais = new ArrayList<JCheckBox>();
+		
+		for (int i = 0; i < numberOfPlayers; ++i){
+			final JTextField name = new JTextField();
+			dialog.add(name);
+			JCheckBox isAI = new JCheckBox("AI");
+			isAI.addActionListener(new ActionListener() {
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					name.setEditable(!name.isEditable());
+				}
+			});
+			dialog.add(isAI);
+			names.add(name);
+			ais.add(isAI);
+		}
+		
+		dialog.add(new JLabel(""));
+		
+		
+		wait = true;
+		
+		JButton submit = new JButton("Done");
+		submit.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				wait = false;
+			}
+		});
+		
+		dialog.add(submit);
+		dialog.setVisible(true);
+		
+		while (wait){
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e1) {
+			}
+		}
+		
+		dialog.dispose();
+		
+		List<String> playerNames = new ArrayList<String>();
+		for (int i = 0; i < numberOfPlayers; ++i){
+			if (!ais.get(i).isSelected()){
+				playerNames.add(names.get(i).getText());
+				System.out.println(names.get(i).getText());
+			}
+		}
+
+		return playerNames;
 	}
 
 	@Override
