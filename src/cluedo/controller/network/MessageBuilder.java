169de27bf6ce82c52347a01ad98e1b52f1e6baa3
Simon Pinfold <mail@synap5e.info>
2014-08-15 19:51:30 +1200

    Documented MessageBuilder

--- src/cluedo/controller/network/MessageBuilder.java
+++ src/cluedo/controller/network/MessageBuilder.java
@@ -1,16 +1,13 @@
 package cluedo.controller.network;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import util.json.JsonBoolean;
 import util.json.JsonEntity;
-import util.json.JsonList;
 import util.json.JsonNumber;
 import util.json.JsonObject;
 import util.json.JsonString;
 
-/**
+/** This class provides an easy way to build a json message to send data (events, requests, responses) 
+ * between the server and client versions of the game
  * 
  * @author Simon Pinfold
  *
@@ -19,43 +16,50 @@ public class MessageBuilder {
 	
 	private JsonObject message = new JsonObject();
 
+	/** Set the type of the message */
 	public MessageBuilder type(String type) {
 		message.put("type", type);
 		return this;
 	}
 
+	/** Set the name of the push/pull event. This should not be set on responses */
 	public MessageBuilder name(String name) {
 		message.put("name", name);
 		return this;
 	}
 
-	public MessageBuilder parameter(String paramName, int i) {
-		return parameter(paramName, new JsonNumber(i));
+	/** Add a parameter to the parameter object of the message*/
+	public MessageBuilder parameter(String paramName, int value) {
+		return parameter(paramName, new JsonNumber(value));
 	}
 	
-	public MessageBuilder parameter(String paramName, String s) {
-		return parameter(paramName, new JsonString(s));
+	/** Add a parameter to the parameter object of the message*/
+	public MessageBuilder parameter(String paramName, String value) {
+		return parameter(paramName, new JsonString(value));
 	}
 	
-	public MessageBuilder parameter(String paramName, boolean b) {
-		return parameter(paramName, new JsonBoolean(b));
+	/** Add a parameter to the parameter object of the message*/
+	public MessageBuilder parameter(String paramName, boolean value) {
+		return parameter(paramName, new JsonBoolean(value));
 	}
 	
-	public MessageBuilder parameter(String paramName, JsonEntity val) {
+	/** Add a parameter to the parameter object of the message*/
+	public MessageBuilder parameter(String paramName, JsonEntity value) {
 		if (!message.containsKey("parameters")){
 			message.put("parameters", new JsonObject());
 		}
 		JsonObject parameters = (JsonObject) message.get("parameters");
-		parameters.put(paramName, val);
+		parameters.put(paramName, value);
 		return this;
 	}
 	
+	/** Set the return value for the message. This should only be used when returning data */
 	public MessageBuilder returnValue(JsonEntity ret) {
 		message.put("return", ret);
 		return this;
 	}
 
-	
+	/** Build the message */
 	public JsonObject build(){
 		return message;
 	}
