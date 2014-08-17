60c4396ffae7725e0a96ca073d14222391df229f
Jamigrith <james@greenwood.net.nz>
2014-08-17 23:33:20 +1200

    Documented all the files in model.card

--- src/cluedo/model/card/Card.java
+++ src/cluedo/model/card/Card.java
@@ -1,9 +1,15 @@
 package cluedo.model.card;
+
 /**
+ * The Card interface represents a Card in the game of Cluedo.
  * 
  * @author Simon Pinfold
  *
  */
 public interface Card {
+	/**
+	 * Get the name of the card
+	 * @return the name
+	 */
 	public String getName();
 }
